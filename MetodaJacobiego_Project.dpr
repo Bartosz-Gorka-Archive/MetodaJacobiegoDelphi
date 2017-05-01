bda1c8b05c436a7acfc35c598ff385b1eacc6f0a
Bartosz Górka <bartosz@kmcstudio.pl>
2017-05-01 12:14:41 +0200

    Create a project file

---
+++ MetodaJacobiego_Project.dpr
@@ -0,0 +1,14 @@
+program MetodaJacobiego_Project;
+
+uses
+  Forms,
+  MainUnit in 'MainUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
